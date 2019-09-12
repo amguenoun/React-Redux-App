0e1fd65e7872902b107aefa54711f89ebb30cd1a
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-12 16:02:52 -0400

    created a reducer and added redux dev tools code

--- poke-tcg/src/index.js
+++ poke-tcg/src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+import App from './App';
+import { reducer } from './store/reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
---
+++ poke-tcg/src/store/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+    isFetching: false,
+    pokemonCards: [],
+    error: '',
+}
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
